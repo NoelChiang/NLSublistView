9c0e25e8ea47fd718d8c85f788d6ac635e7c4146
Noel <valkyrie7266@hotmail.com>
2019-01-22 14:07:38 +0800

    Initial Commit

---
+++ UI_DetailListTableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI_DetailListTableview
+//
+//  Created by Noel on 2019/1/22.
+//  Copyright © 2019 Noel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
